9c660ccaac68dd59c9be962c9093979694d92e59
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2023-12-27 16:56:48 -0300

    Add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+
 import "./globals.css";
+import Navbar from "./components/Navbar";
+
+export const revalidate = 3600;
 
 export const metadata: Metadata = {
   title: "Next.js Image gallery",
@@ -14,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Navbar />
         <main className="max-w-6xl mx-auto">{children}</main>
       </body>
     </html>
